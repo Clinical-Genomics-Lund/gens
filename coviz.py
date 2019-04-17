5e8c1bb3e22a74772367e281c02c0ae9e8352773
Ronja Grosz <ronja.grosz@gmail.com>
2019-04-17 10:59:22 +0200

    Pep8 syntax formatted
     * Also removed trailing white spaces

--- coviz.py
+++ coviz.py
@@ -1,65 +1,83 @@
-import tabix
+'''
+Whole genome visualization of BAF and log R ratio
+'''
+
+import sys
 import json
-from flask import Flask, request, session, g, redirect, url_for, abort, render_template, flash, jsonify, send_from_directory, make_response
-app = Flask(__name__)
+import tabix
+from flask import Flask, request, render_template, jsonify
+APP = Flask(__name__)
 
 
-@app.route('/', methods=['POST', 'GET'])
+@APP.route('/', methods=['POST', 'GET'])
 def coverage_view():
-        region = request.form.get('region', '1:100000-200000')
+    '''
+    Method for displaying a region
+    '''
+    region = request.form.get('region', '1:100000-200000')
 
-        call_chrom = 1
-        call_start = 1011000
-        call_end   = 1015000
-        res, chrom, start_pos, end_pos = parse_region_str(region)
-        cov_file = "/trannel/proj/wgs/sentieon/bam/merged.cov.gz"
-        tb = tabix.open(cov_file)
-        records = list(tb.query(res+'_'+chrom, int(start_pos), int(end_pos)))
+    call_chrom = 1
+    call_start = 1011000
+    call_end = 1015000
+    res, chrom, start_pos, end_pos = parse_region_str(region)
+    cov_file = "/trannel/proj/wgs/sentieon/bam/merged.cov.gz"
+    tb_file = tabix.open(cov_file)
+    records = list(tb_file.query(res+'_'+chrom, int(start_pos), int(end_pos)))
 
-        baf_file = "/trannel/proj/wgs/sentieon/bam/BAF.bed.gz"
-        baf = tabix.open(baf_file)
-        baf_records = list(baf.query(chrom, int(start_pos), int(end_pos)))
-        
-        return render_template('show_cov.html', data=json.dumps(records), baf=json.dumps(baf_records), chrom=chrom, start=start_pos, end=end_pos, call_chrom=call_chrom, call_start=call_start, call_end=call_end)
+    baf_file = "/trannel/proj/wgs/sentieon/bam/BAF.bed.gz"
+    baf = tabix.open(baf_file)
+    baf_records = list(baf.query(chrom, int(start_pos), int(end_pos)))
+
+    return render_template('show_cov.html', data=json.dumps(records),
+                           baf=json.dumps(baf_records), chrom=chrom,
+                           start=start_pos, end=end_pos, call_chrom=call_chrom,
+                           call_start=call_start, call_end=call_end)
 
 
-@app.route('/_getcov', methods=['GET'])
+@APP.route('/_getcov', methods=['GET'])
 def get_cov():
-        region = request.args.get('region', '1:100000-200000')
+    '''
+    Method for redrawing region on button change
+    '''
+    region = request.args.get('region', '1:100000-200000')
 
-        res, chrom, start_pos, end_pos = parse_region_str(region)        
-        cov_file = "/trannel/proj/wgs/sentieon/bam/merged.cov.gz"
-        tb = tabix.open(cov_file)
-        records = list(tb.query(res+'_'+chrom, int(start_pos), int(end_pos)))
+    res, chrom, start_pos, end_pos = parse_region_str(region)
+    cov_file = "/trannel/proj/wgs/sentieon/bam/merged.cov.gz"
+    tb_file = tabix.open(cov_file)
+    records = list(tb_file.query(res+'_'+chrom, int(start_pos), int(end_pos)))
 
-        baf_file = "/trannel/proj/wgs/sentieon/bam/BAF.bed.gz"
-        baf = tabix.open(baf_file)
-        baf_records = list(baf.query(chrom, int(start_pos), int(end_pos)))
-       
-        return jsonify(data=records, baf=baf_records, status="ok", chrom=chrom, start=start_pos, end=end_pos)
+    baf_file = "/trannel/proj/wgs/sentieon/bam/BAF.bed.gz"
+    baf = tabix.open(baf_file)
+    baf_records = list(baf.query(chrom, int(start_pos), int(end_pos)))
+
+    return jsonify(data=records, baf=baf_records, status="ok", chrom=chrom,
+                   start=start_pos, end=end_pos)
 
 def parse_region_str(region):
-        chrom =""
+    '''
+    Parses a region string
+    '''
+    chrom = ""
+    start_pos = 0
+    end_pos = 0
+    if ":" in region and "-" in region:
+        chrom, pos_range = region.split(':')
+        start_pos, end_pos = pos_range.split('-')
+    else:
+        chrom, start_pos, end_pos = region.split()
+
+    if "chr" in chrom:
+        chrom.lstrip('chr')
+    if int(start_pos) < 0:
         start_pos = 0
-        end_pos = 0
-        if ":" in region and "-" in region:
-                chrom, pos_range = region.split(':')
-                start_pos, end_pos = pos_range.split('-')
-        else:
-                chrom, start_pos, end_pos = region.split()
+    size = int(end_pos) - int(start_pos)
 
-        if "chr" in chrom:
-                chrom.lstrip('chr')
-        if int(start_pos) < 0:
-                start_pos = 0
-        size = int(end_pos) - int(start_pos)
+    resolution = "d"
+    if size > 25000000:
+        resolution = "a"
+    elif size > 3000000:
+        resolution = "b"
+    elif size > 200000:
+        resolution = "c"
 
-        resolution = "d"
-        if size > 25000000:
-                resolution = "a"
-        elif size > 3000000:
-                resolution = "b"
-        elif size > 200000:
-                resolution = "c"
-                
-        return resolution, chrom, start_pos, end_pos
+    return resolution, chrom, start_pos, end_pos
