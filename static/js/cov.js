38c2bc659f81aff5c40220f044282c907ecdba0b
Ronja Grosz <ronja.grosz@gmail.com>
2019-05-07 15:41:34 +0200

    Moved draw function to respectively classes
    * The two different classes now own the draw functions

--- static/js/cov.js
+++ static/js/cov.js
@@ -58,8 +58,8 @@ function drawRotatedText (ctx, text, posx, posy) {
   ctx.restore();
 }
 
-class GeneCanvas {
-  constructor (canvasWidth, canvasHeight) {
+class GeneCanvas { // eslint-disable-line no-unused-vars
+  constructor (canvasWidth, canvasHeight, chromosome, start, end) {
     // Canvas variables
     this.cvar = {
       // Box values
@@ -81,11 +81,17 @@ class GeneCanvas {
       logr_start: 4.0,
       logr_end: -4.0,
       logr_frac: 1.0,
-      logr_padding: 0
+      logr_padding: 0,
+
+      chromosome: chromosome,
+      start: start,
+      end: end,
+      disallowDrag: false
     };
     this.cvar.box_width -= this.cvar.leftPadding;
     this.cvar.box_height = (canvasHeight - this.cvar.topOffset - this.cvar.baf_padding) / 2;
     this.cvar.logr_padding = this.cvar.baf_padding + this.cvar.box_height + 20;
+
 
     // Create canvas for data
     this.dataCanvas = createCanvas(canvasWidth, canvasHeight, 'interactive-container');
@@ -124,10 +130,67 @@ class GeneCanvas {
     drawRotatedText(ctx, 'Log R Ratio', 10,
       this.cvar.logr_padding + this.cvar.box_height / 2);
   }
+
+  // Draw coverage for gene canvas
+  drawCoverage (data, baf) {
+    let ch = data[0][0];
+    let titleLength = 110;
+    let leftPadding = 0;
+
+    // Draw on empty temporary canvas
+    let ctx = this.drawCanvas.getContext('2d');
+    let canvasWidth = this.drawCanvas.width;
+    let canvasHeight = this.drawCanvas.height;
+
+    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
+    drawTitle(this.staticCanvas.getContext('2d'), this.cvar,
+        'Chromosome ' + this.cvar.chromosome, titleLength);
+    drawXAxis(ctx, this.cvar, canvasWidth);
+    console.log(ch, this.cvar.start, this.cvar.end, (this.cvar.end - this.cvar.start), data.length);
+
+    // Draw BAF values
+    let ampl = this.cvar.box_height;
+    let padding = this.cvar.baf_padding + this.cvar.box_height;
+    let scale = canvasWidth / (this.cvar.end - this.cvar.start);
+    ctx.fillStyle = '#FF0000';
+    for (let i = 0; i < baf.length - 1; i++) {
+      ctx.fillRect(leftPadding + scale * (baf[i][1] - this.cvar.start),
+          padding - ampl * baf[i][3], 2, 2);
+    }
+
+    ctx.fillStyle = '#000000';
+    if (this.cvar.chromosome === callChrom && (this.cvar.start < callEnd && this.cvar.end > callStart)) {
+      ctx.fillRect(leftPadding + scale * (callStart - this.cvar.start),
+          120, scale * (callEnd - callStart), this.cvar.topOffset);
+      console.log('DRAW_CALL');
+    }
+
+    // Draw Log R ratio values
+    ampl = this.cvar.box_height / (2 * this.cvar.logr_start);
+    padding = this.cvar.logr_padding + this.cvar.box_height / 2;
+    if (data.length > 1000) {
+      for (let i = 0; i < data.length - 1; i++) {
+        ctx.fillRect(leftPadding + scale * (data[i][1] - this.cvar.start),
+            padding - ampl * data[i][3], 2, 2);
+      }
+    } else {
+      ctx.beginPath();
+      ctx.moveTo(this.cvar.leftPadding, padding - ampl * data[0][3]);
+      for (let i = 1; i < data.length - 1; i++) {
+        ctx.lineTo(leftPadding + scale * (data[i][1] - this.cvar.start),
+            padding - ampl * data[i][3], 2, 2);
+      }
+      ctx.stroke();
+    }
+
+    this.dataCanvas.getContext('2d').clearRect(0, 0, this.dataCanvas.width,
+        this.dataCanvas.height);
+    this.dataCanvas.getContext('2d').drawImage(this.drawCanvas, 0, 0);
+  }
 }
 
-class OverviewCanvas {
-  constructor (canvasWidth, canvasHeight) {
+class OverviewCanvas { // eslint-disable-line no-unused-vars
+  constructor (canvasWidth, canvasHeight, chromosome) {
     // Canvas variables
     this.cvar = {
       // Box values
@@ -149,7 +212,12 @@ class OverviewCanvas {
       logr_start: 4.0,
       logr_end: -4.0,
       logr_frac: 1.0,
-      logr_padding: 0
+      logr_padding: 0,
+
+      chromosome: chromosome,
+      start: 0,
+      end: 35460000
+
     };
     this.cvar.box_width -= this.cvar.leftPadding;
     this.cvar.box_height = (canvasHeight - this.cvar.topOffset - this.cvar.baf_padding) / 2;
@@ -163,10 +231,61 @@ class OverviewCanvas {
 
     // Draw BAF context
     drawBoundingBox(ctx, this.cvar, this.cvar.baf_frac,
-      this.cvar.baf_padding, this.cvar.topOffset);
+        this.cvar.baf_padding, this.cvar.topOffset);
     // Draw LogR context
     drawBoundingBox(ctx, this.cvar, this.cvar.logr_frac,
-      this.cvar.logr_padding, 0);
+        this.cvar.logr_padding, 0);
+  }
+
+  // Draw coverage for overview canvas
+  drawCoverage (data, baf) {
+    let ch = data[0][0];
+    let titleLength = 12;
+
+    // Draw on empty temporary canvas
+    let ctx = this.staticCanvas.getContext('2d');
+    let canvasWidth = this.staticCanvas.width;
+    let canvasHeight = this.staticCanvas.height;
+
+    drawTitle(ctx, this.cvar, this.cvar.chromosome, titleLength);
+    console.log(ch, this.cvar.start, this.cvar.end,
+        (this.cvar.end - this.cvar.start), data.length);
+
+    // Draw BAF values
+    let ampl = this.cvar.box_height;
+    let padding = this.cvar.baf_padding + this.cvar.box_height;
+    let scale = canvasWidth / (this.cvar.end - this.cvar.start);
+    ctx.fillStyle = '#FF0000';
+    for (let i = 0; i < baf.length - 1; i++) {
+      ctx.fillRect(this.cvar.leftPadding + scale * (baf[i][1] - this.cvar.start),
+          padding - ampl * baf[i][3], 2, 2);
+    }
+
+    ctx.fillStyle = '#000000';
+    if (this.cvar.chromosome === callChrom &&
+        (this.cvar.start < callEnd && this.cvar.end > callStart)) {
+      ctx.fillRect(this.cvar.leftPadding + scale * (callStart - this.cvar.start),
+          120, scale * (callEnd - callStart), this.cvar.topOffset);
+      console.log('DRAW_CALL');
+    }
+
+    // Draw Log R ratio values
+    ampl = this.cvar.box_height / (2 * this.cvar.logr_start);
+    padding = this.cvar.logr_padding + this.cvar.box_height / 2;
+    if (data.length > 1000) {
+      for (let i = 0; i < data.length - 1; i++) {
+        ctx.fillRect(this.cvar.leftPadding + scale * (data[i][1] - this.cvar.start),
+            padding - ampl * data[i][3], 2, 2);
+      }
+    } else {
+      ctx.beginPath();
+      ctx.moveTo(this.cvar.leftPadding, padding - ampl * data[0][3]);
+      for (let i = 1; i < data.length - 1; i++) {
+        ctx.lineTo(this.cvar.leftPadding + scale * (data[i][1] - this.cvar.start),
+            padding - ampl * data[i][3], 2, 2);
+      }
+      ctx.stroke();
+    }
   }
 }
 
@@ -179,29 +298,29 @@ function drawTitle (ctx, cvar, title, titleLength) {
 }
 
 function drawXAxis (ctx, cvar, canvasWidth) {
-  let scale = canvasWidth / (end - start);
-  let xAxisTickFrq = Math.pow(10, (end - start).toString().length - 2);
-  let xAxisTick = Math.ceil(start / xAxisTickFrq) * xAxisTickFrq;
+  let scale = canvasWidth / (cvar.end - cvar.start);
+  let xAxisTickFrq = Math.pow(10, (cvar.end - cvar.start).toString().length - 2);
+  let xAxisTick = Math.ceil(cvar.start / xAxisTickFrq) * xAxisTickFrq;
   let xAxisOffset = 10; // Offset from top padding
   let everyOther = false;
   ctx.font = '9px Arial';
 
-  if (((end - start) / xAxisTickFrq) > 15) {
+  if (((cvar.end - cvar.start) / xAxisTickFrq) > 15) {
     everyOther = true;
   }
 
   // Draw x-axis tick value
   let counter = 0;
   let prevXPos = 0;
-  while (xAxisTick < end) {
-    ctx.fillRect(scale * (xAxisTick - start),
-      cvar.baf_padding - 2,
-      2, 5);
+  while (xAxisTick < cvar.end) {
+    ctx.fillRect(scale * (xAxisTick - cvar.start),
+        cvar.baf_padding - 2,
+        2, 5);
     counter++;
     // Only draw value on every other tick
     let txt = numberWithCommas(xAxisTick);
     let txtWidth = ctx.measureText(txt).width;
-    let tickXPos = scale * (xAxisTick - start) - txtWidth / 2;
+    let tickXPos = scale * (xAxisTick - cvar.start) - txtWidth / 2;
     if ((!everyOther || counter % 2 === 0) && (tickXPos - prevXPos) > (txtWidth + 5)) {
       ctx.fillText(txt, tickXPos, cvar.baf_padding - xAxisOffset);
       prevXPos = tickXPos;
@@ -210,111 +329,41 @@ function drawXAxis (ctx, cvar, canvasWidth) {
   }
 }
 
-function drawCoverage (data, baf, canvas, chromosome) {
-  let ch = data[0][0];
-  let interactive = !!canvas.drawCanvas;
-  let covStart = interactive ? start : chromStart;
-  let covEnd = interactive ? end : chromEnd;
-  let titleLength = interactive ? 110 : 12;
-  let leftPadding = interactive ? 0 : canvas.cvar.leftPadding;
-
-  // Draw on empty temporary canvas
-  let ctx = interactive ? canvas.drawCanvas.getContext('2d')
-    : canvas.staticCanvas.getContext('2d');
-  let canvasWidth = interactive ? canvas.drawCanvas.width
-    : canvas.staticCanvas.width;
-  let canvasHeight = interactive ? canvas.drawCanvas.height
-    : canvas.staticCanvas.height;
-
-  // Only draw title and x-axis values for interactive canvas
-  if (interactive) {
-    ctx.clearRect(0, 0, canvasWidth, canvasHeight);
-    drawTitle(canvas.staticCanvas.getContext('2d'), canvas.cvar,
-      'Chromosome ' + chromosome, titleLength);
-    drawXAxis(ctx, canvas.cvar, canvasWidth);
-  } else {
-    drawTitle(ctx, canvas.cvar, chromosome, titleLength);
-  }
-  console.log(ch, covStart, covEnd, (covEnd - covStart), data.length);
-
-  // Draw BAF values
-  let ampl = canvas.cvar.box_height;
-  let padding = canvas.cvar.baf_padding + canvas.cvar.box_height;
-  let scale = canvasWidth / (covEnd - covStart);
-  ctx.fillStyle = '#FF0000';
-  for (let i = 0; i < baf.length - 1; i++) {
-    ctx.fillRect(leftPadding + scale * (baf[i][1] - covStart),
-      padding - ampl * baf[i][3], 2, 2);
-  }
-
-  ctx.fillStyle = '#000000';
-  if (chrom === callChrom && (covStart < callEnd && covEnd > callStart)) {
-    ctx.fillRect(leftPadding + scale * (callStart - covStart),
-      120, scale * (callEnd - callStart), canvas.cvar.topOffset);
-    console.log('DRAW_CALL');
-  }
-
-  // Draw Log R ratio values
-  ampl = canvas.cvar.box_height / (2 * canvas.cvar.logr_start);
-  padding = canvas.cvar.logr_padding + canvas.cvar.box_height / 2;
-  if (data.length > 1000) {
-    for (let i = 0; i < data.length - 1; i++) {
-      ctx.fillRect(leftPadding + scale * (data[i][1] - covStart),
-        padding - ampl * data[i][3], 2, 2);
-    }
-  } else {
-    ctx.beginPath();
-    ctx.moveTo(canvas.cvar.leftPadding, padding - ampl * data[0][3]);
-    for (let i = 1; i < data.length - 1; i++) {
-      ctx.lineTo(leftPadding + scale * (data[i][1] - covStart),
-        padding - ampl * data[i][3], 2, 2);
-    }
-    ctx.stroke();
-  }
-
-  if (interactive) {
-    canvas.dataCanvas.getContext('2d').clearRect(0, 0,
-      canvas.dataCanvas.width,
-      canvas.dataCanvas.height);
-    canvas.dataCanvas.getContext('2d').drawImage(canvas.drawCanvas, 0, 0);
-  }
-}
-
 function left () {
-  let size = end - start;
-  start -= Math.floor(0.1 * size);
-  end -= Math.floor(0.1 * size);
+  let size = gc.cvar.end - gc.cvar.start;
+  gc.cvar.start -= Math.floor(0.1 * size);
+  gc.cvar.end -= Math.floor(0.1 * size);
   redraw();
 }
 function right () {
-  let size = end - start;
-  start += Math.floor(0.1 * size);
-  end += Math.floor(0.1 * size);
+  let size = gc.cvar.end - gc.cvar.start;
+  gc.cvar.start += Math.floor(0.1 * size);
+  gc.cvar.end += Math.floor(0.1 * size);
   redraw();
 }
 function zoomIn () {
-  let size = end - start;
-  start += Math.floor(size * 0.25);
-  end -= Math.floor(size * 0.25);
+  let size = gc.cvar.end - gc.cvar.start;
+  gc.cvar.start += Math.floor(size * 0.25);
+  gc.cvar.end -= Math.floor(size * 0.25);
   redraw();
 }
 function zoomOut () {
-  let size = end - start;
-  start -= Math.floor(size * 0.5);
-  end += Math.floor(size * 0.5);
-  if (start < 1) {
-    start = 1;
+  let size = gc.cvar.end - gc.cvar.start;
+  gc.cvar.start -= Math.floor(size * 0.5);
+  gc.cvar.end += Math.floor(size * 0.5);
+  if (gc.cvar.start < 1) {
+    gc.cvar.start = 1;
   }
   redraw();
 }
 
 function redraw () {
   $.getJSON($SCRIPT_ROOT + '/_getcov', {
-    region: chrom + ':' + start + '-' + end,
+    region: gc.cvar.chromosome + ':' + gc.cvar.start + '-' + gc.cvar.end,
     median: logRMedian
   }, function (result) {
-    drawCoverage(result['data'], result['baf'], gc, chrom);
-  }).done(function () { disallowDrag = false; });
+    gc.drawCoverage(result['data'], result['baf']);
+  }).done(function () { gc.cvar.disallowDrag = false; });
 }
 
 function numberWithCommas (x) {
@@ -347,7 +396,7 @@ function keyMapper (options) {
       // Enter was pressed, process previous key presses.
       if (state.buffer < 24 && state.buffer > 0) {
         // Display new chromosome
-        chrom = state.buffer;
+        gc.cvar.chromosome = state.buffer;
         redraw();
       }
     } else if (!isFinite(key)) {
