724318b5e7866bbdbcbc55cd69046128df8d91f1
Ronja Grosz <ronja.grosz@gmail.com>
2019-10-16 11:12:46 +0200

    Fixed submit button

--- main.mjs
+++
@@ -1,5 +0,0 @@
-import * as interactive from './modules/interactive.mjs';
-import * as overview from './modules/overview.mjs';
-
-export function main (logRMedian, title, callChrom, callStart, callEnd) {
-}
