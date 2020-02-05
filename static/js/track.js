f0bdc9000a65f3e69c1590f16022e268238d1f40
Ronja Grosz <ronja.grosz@gmail.com>
2020-02-05 11:17:15 +0100

    Broke up tracks into classes
    * Transcript extends track
    * Makes it possibly to reuse a lot for annotations

--- static/js/track.js
+++ static/js/track.js
@@ -1,5 +1,5 @@
-class TrackCanvas {
-  constructor (x, width, near, far) {
+class Track {
+  constructor (width, near, far, maxRows, visibleHeight, minHeight) {
     // Track variables
     this.featureHeight = 20; // Max height for feature
     this.featureMargin = 14; // Margin for fitting gene name under track
@@ -11,22 +11,28 @@ class TrackCanvas {
 
     // Dimensions of track canvas
     this.width = width; // Width of canvas
-    this.maxHeight = this.tracksYPos(67); // Max height of canvas, height_order <= 66
-    this.visibleHeight = 100; // Visible height for expanded canvas, overflows for scroll
-    this.minHeight = 35; // Minimized height
+    this.maxHeight = this.tracksYPos(maxRows); // Max height of canvas, height_order <= 66
+    this.visibleHeight = visibleHeight; // Visible height for expanded canvas, overflows for scroll
+    this.minHeight = minHeight; // Minimized height
 
     // Canvases
     this.drawCanvas = new OffscreenCanvas(this.width, this.maxHeight);
     this.context = this.drawCanvas.getContext('webgl2');
-    this.trackCanvas = document.getElementById('track-canvas');
-    this.trackContext = this.trackCanvas.getContext('2d');
+    this.trackCanvas = null; // Set in parent class
+    this.trackTitle = null; // Set in parent class
+    this.trackContainer = null; // Set in parent class
+  }
+
+  setupHTML(xPos) {
+    // Setup container
+    this.trackContainer.style.marginLeft = xPos + 'px';
 
     // Setup initial track Canvas
+    this.trackContext = this.trackCanvas.getContext('2d');
     this.trackCanvas.width = this.width;
     this.trackCanvas.height = this.minHeight;
 
     // Setup track div
-    this.trackTitle = document.getElementById('track-titles');
     this.trackTitle.style.width = this.width + 'px';
     this.trackTitle.style.height = this.minHeight + 'px';
 
@@ -46,15 +52,14 @@ class TrackCanvas {
       this.maxHeight / 2 - lineMargin, 1);
 
     // Setup context menu
-    this.trackContainer = document.getElementById('track-container');
     this.trackContainer.addEventListener('contextmenu',
-      function(event) {
+      (event) => {
         event.preventDefault();
 
         // Toggle between expanded/collapsed view
-        tc.expanded = !tc.expanded;
-        tc.clearTracks();
-        tc.drawTracks(inputField.placeholder);
+        this.expanded = !this.expanded;
+        this.clearTracks();
+        this.drawTracks(inputField.placeholder);
       }, false);
   }
 
@@ -63,7 +68,7 @@ class TrackCanvas {
     this.trackContext.clearRect(0, 0, this.trackCanvas.width, this.trackCanvas.height);
 
     // Clear tooltip titles
-    $('#track-titles').empty();
+    $('#' + this.trackTitle.id).empty();
   }
 
   drawGeneName(geneName, xPos, yPos, textHeight) {
@@ -86,126 +91,15 @@ class TrackCanvas {
   }
 
   insertTitle(text, left, top, width, height, zIndex) {
-		let title = document.createElement('div');
-		title.title = text;
-		title.style.left = left;
-		title.style.top = top;
-		title.style.width = width;
-		title.style.height = height;
-		title.style.position = 'absolute';
-		title.style.zIndex = zIndex;
-		this.trackTitle.appendChild(title);
-  }
-
-  drawTracks (region) {
-    $.getJSON($SCRIPT_ROOT + '/_gettrackdata', {
-      region: region,
-      width: this.trackCanvas.width,
-    }, function(result) {
-      const scale = tc.trackCanvas.width / (result['end_pos'] - result['start_pos']);
-      const titleMargin = 2;
-
-      // Set needed height of visible canvas and transcript tooltips
-      if (result['max_height_order'] == 0) {
-        // No results, do not show tracks
-        tc.trackCanvas.height = 0;
-        tc.trackTitle.style.height = 0 + 'px';
-        tc.trackContainer.style.height = 0 + 'px';
-      } else if (tc.expanded) {
-        const maxYPos = tc.tracksYPos(result['max_height_order'] + 1);
-        tc.trackCanvas.height = maxYPos;
-        tc.trackTitle.style.height = maxYPos + 'px';
-        tc.trackContainer.style.height = tc.visibleHeight + 'px';
-      } else {
-        tc.trackCanvas.height = tc.minHeight;
-        tc.trackTitle.style.height = tc.minHeight + 'px';
-        tc.trackContainer.style.height = tc.minHeight + 'px';
-      }
-
-      // Go through results and draw appropriate symbols
-      for (let i = 0; i < result['tracks'].length; i++) {
-        const track = result['tracks'][i];
-        const geneName = track['gene_name'];
-        const transcriptID = track['transcript_id'];
-        const seqname = track['seqname'];
-        const height_order = track['height_order'];
-        const strand = track['strand'];
-        const start = track['start'];
-        const end = track['end'];
-
-        // Only draw visible tracks
-        if (!tc.expanded && height_order != 1)
-          continue
-
-        const adjustedYPos = tc.tracksYPos(height_order);
-        const textHeight = 10;
-
-        tc.drawTrackLen(scale * (start - result['start_pos']),
-          scale * (end - result['start_pos']), adjustedYPos);
-
-        // Draw gene name
-        if (result['res'] == 'd') {
-          tc.drawGeneName(geneName,
-            scale * ((start + end) / 2 - result['start_pos']),
-            adjustedYPos + tc.featureHeight, textHeight);
-        }
-
-        // Add tooltip title for whole gene
-        const geneText = geneName + '\n' + 'chr' + seqname + ':' + start + '-' + end + '\n' + 'id = ' + transcriptID;
-        tc.insertTitle(geneText,
-          titleMargin + scale * (start - result['start_pos']) + 'px',
-          titleMargin + adjustedYPos - tc.featureHeight / 2 + 'px',
-          scale * (end - start) + 'px',
-          tc.featureHeight + textHeight + 'px',
-          0);
-
-        let latestFeaturePos = start;
-        for (let j = 0; j < track['features'].length; j++) {
-          let feature = track['features'][j];
-
-          // Draw arrows
-          let diff = feature['start'] - latestFeaturePos;
-          if (scale * diff >= tc.arrowWidth) {
-            let direction = strand == '+' ? 1 : -1;
-            tc.drawArrow(scale * (latestFeaturePos - result['start_pos'] + diff / 2),
-              adjustedYPos, direction, tc.featureHeight / 2);
-          }
-          latestFeaturePos = feature['end'];
-
-          switch(feature['feature']) {
-            case 'exon':
-              let exonText = geneText + '\n' + '-'.repeat(30) + '\nExon number: ' + feature['exon_number'] +
-                '\nchr' + seqname + ':' + feature['start'] + '-' + feature['end'];
-
-              // Add tooltip title for whole gene
-              tc.insertTitle(exonText,
-                titleMargin + scale * (feature['start'] - result['start_pos']) + 'px',
-                titleMargin + adjustedYPos - tc.featureHeight / 2 + 'px',
-                scale * (feature['end'] - feature['start']) + 'px',
-                tc.featureHeight + 'px',
-                1);
-
-              tc.drawBand(scale * (feature['start'] - result['start_pos']),
-                adjustedYPos, scale * (feature['end'] - feature['start']), tc.featureHeight);
-              break;
-            case 'three_prime_utr':
-              tc.drawBand(scale * (feature['start'] - result['start_pos']),
-                adjustedYPos, scale * (feature['end'] - feature['start']), tc.featureHeight / 2);
-              break;
-          }
-        }
-      }
-
-      tc.renderer.render(tc.scene, tc.camera);
-
-      // Transfer image to visible canvas
-      tc.trackContext.drawImage(tc.drawCanvas.transferToImageBitmap(),
-        0, 0, tc.trackCanvas.width, tc.trackCanvas.height,
-        0, 0, tc.trackCanvas.width, tc.trackCanvas.height);
-
-      // Clear draw canvas
-      ic.scene.remove.apply(tc.scene, tc.scene.children);
-    })
+    let title = document.createElement('div');
+    title.title = text;
+    title.style.left = left;
+    title.style.top = top;
+    title.style.width = width;
+    title.style.height = height;
+    title.style.position = 'absolute';
+    title.style.zIndex = zIndex;
+    this.trackTitle.appendChild(title);
   }
 
   drawTrackLen (xStart, xStop, yPos) {
---
+++ static/js/transcript.js
@@ -0,0 +1,129 @@
+class Transcript extends Track {
+  constructor (x, width, near, far) {
+    // Dimensions of track canvas
+    const maxRows = 67; // Max height order for canvas
+    const visibleHeight = 100; // Visible height for expanded canvas, overflows for scroll
+    const minHeight = 35; // Minimized height
+
+    super(width, near, far, maxRows, visibleHeight, minHeight);
+
+    // Set inherited variables
+    this.trackCanvas = document.getElementById('track-canvas');
+    this.trackTitle = document.getElementById('track-titles');
+    this.trackContainer = document.getElementById('track-container');
+
+    // Setup html objects now that we have gotten the canvas and div elements
+    this.setupHTML(x);
+  }
+
+  drawTracks (region) {
+    $.getJSON($SCRIPT_ROOT + '/_gettrackdata', {
+      region: region,
+      width: this.trackCanvas.width,
+    }, (result) => {
+      const scale = this.trackCanvas.width / (result['end_pos'] - result['start_pos']);
+      const titleMargin = 2;
+
+      // Set needed height of visible canvas and transcript tooltips
+      if (result['max_height_order'] == 0) {
+        // No results, do not show tracks
+        this.trackCanvas.height = 0;
+        this.trackTitle.style.height = 0 + 'px';
+        this.trackContainer.style.height = 0 + 'px';
+      } else if (this.expanded) {
+        const maxYPos = this.tracksYPos(result['max_height_order'] + 1);
+        this.trackCanvas.height = maxYPos;
+        this.trackTitle.style.height = maxYPos + 'px';
+        this.trackContainer.style.height = this.visibleHeight + 'px';
+      } else {
+        this.trackCanvas.height = this.minHeight;
+        this.trackTitle.style.height = this.minHeight + 'px';
+        this.trackContainer.style.height = this.minHeight + 'px';
+      }
+
+      // Go through results and draw appropriate symbols
+      for (let i = 0; i < result['tracks'].length; i++) {
+        const track = result['tracks'][i];
+        const geneName = track['gene_name'];
+        const transcriptID = track['transcript_id'];
+        const seqname = track['seqname'];
+        const height_order = track['height_order'];
+        const strand = track['strand'];
+        const start = track['start'];
+        const end = track['end'];
+
+        // Only draw visible tracks
+        if (!this.expanded && height_order != 1)
+          continue
+
+        const adjustedYPos = this.tracksYPos(height_order);
+        const textHeight = 10;
+
+        this.drawTrackLen(scale * (start - result['start_pos']),
+          scale * (end - result['start_pos']), adjustedYPos);
+
+        // Draw gene name
+        if (result['res'] == 'd') {
+          this.drawGeneName(geneName,
+            scale * ((start + end) / 2 - result['start_pos']),
+            adjustedYPos + this.featureHeight, textHeight);
+        }
+
+        // Add tooltip title for whole gene
+        const geneText = geneName + '\n' + 'chr' + seqname + ':' + start + '-' + end + '\n' + 'id = ' + transcriptID;
+        this.insertTitle(geneText,
+          titleMargin + scale * (start - result['start_pos']) + 'px',
+          titleMargin + adjustedYPos - this.featureHeight / 2 + 'px',
+          scale * (end - start) + 'px',
+          this.featureHeight + textHeight + 'px',
+          0);
+
+        let latestFeaturePos = start;
+        for (let j = 0; j < track['features'].length; j++) {
+          let feature = track['features'][j];
+
+          // Draw arrows
+          let diff = feature['start'] - latestFeaturePos;
+          if (scale * diff >= this.arrowWidth) {
+            let direction = strand == '+' ? 1 : -1;
+            this.drawArrow(scale * (latestFeaturePos - result['start_pos'] + diff / 2),
+              adjustedYPos, direction, this.featureHeight / 2);
+          }
+          latestFeaturePos = feature['end'];
+
+          switch(feature['feature']) {
+            case 'exon':
+              let exonText = geneText + '\n' + '-'.repeat(30) + '\nExon number: ' + feature['exon_number'] +
+                '\nchr' + seqname + ':' + feature['start'] + '-' + feature['end'];
+
+              // Add tooltip title for whole gene
+              this.insertTitle(exonText,
+                titleMargin + scale * (feature['start'] - result['start_pos']) + 'px',
+                titleMargin + adjustedYPos - this.featureHeight / 2 + 'px',
+                scale * (feature['end'] - feature['start']) + 'px',
+                this.featureHeight + 'px',
+                1);
+
+              this.drawBand(scale * (feature['start'] - result['start_pos']),
+                adjustedYPos, scale * (feature['end'] - feature['start']), this.featureHeight);
+              break;
+            case 'three_prime_utr':
+              this.drawBand(scale * (feature['start'] - result['start_pos']),
+                adjustedYPos, scale * (feature['end'] - feature['start']), this.featureHeight / 2);
+              break;
+          }
+        }
+      }
+
+      this.renderer.render(this.scene, this.camera);
+
+      // Transfer image to visible canvas
+      this.trackContext.drawImage(this.drawCanvas.transferToImageBitmap(),
+        0, 0, this.trackCanvas.width, this.trackCanvas.height,
+        0, 0, this.trackCanvas.width, this.trackCanvas.height);
+
+      // Clear draw canvas
+      ic.scene.remove.apply(this.scene, this.scene.children);
+    });
+  }
+}
