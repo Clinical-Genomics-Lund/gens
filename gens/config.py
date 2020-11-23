5dbd1c7a10ab700b4a8ae1520421bf9524ba6faf
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2020-11-23 09:03:08 +0100

    updated motif colors

--- gens/config.py
+++ gens/config.py
@@ -4,6 +4,6 @@ HG19_PATH = "/access/wgs/plotdata"
 HG38_PATH = "/access/wgs/plot_data"
 # Colors for the UI
 UI_COLORS = {
-    "variants": {"deletion": "#55a868", "duplication": "#dd8452"},
-    "transcripts": {"strand_pos": "#4c72b0", "strand_neg": "#c44e52"},
+    "variants": {"deletion": "#F94144", "duplication": "#277DA1"},
+    "transcripts": {"strand_pos": "#4D908E", "strand_neg": "#43AA8B"},
 }
