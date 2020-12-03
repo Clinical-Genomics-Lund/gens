0f3bb4291376d7f19cb7da6501ffa8c5f765518e
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2020-12-03 11:27:26 +0100

    Fixed issue when getting annotation sources

--- gens/api.py
+++ gens/api.py
@@ -6,8 +6,12 @@ from flask import abort, current_app, jsonify, request
 
 from .db import RecordType, init_database, query_records_in_region
 from .exceptions import RegionParserException
-from .graph import (REQUEST, get_overview_cov, overview_chrom_dimensions,
-                    parse_region_str)
+from .graph import (
+    REQUEST,
+    get_overview_cov,
+    overview_chrom_dimensions,
+    parse_region_str,
+)
 from .io import get_tabix_files
 
 LOG = logging.getLogger(__name__)
@@ -18,18 +22,20 @@ def get_overview_chrom_dim(x_pos, y_pos, plot_width, hg_type):
     Returns the dimensions of all chromosome graphs in screen coordinates
     for drawing the chromosomes correctly in the overview graph
     """
-    LOG.info(f"Get overview chromosome dim: ({x_pos}, {y_pos}), w={plot_width}, {hg_type}")
+    LOG.info(
+        f"Get overview chromosome dim: ({x_pos}, {y_pos}), w={plot_width}, {hg_type}"
+    )
     chrom_dims = overview_chrom_dimensions(x_pos, y_pos, plot_width, hg_type)
     return jsonify(status="ok", chrom_dims=chrom_dims)
 
 
-def get_annotation_sources(hg_type=None):
+def get_annotation_sources(hg_type):
     """
     Returns available annotation source files
     """
     with current_app.app_context():
         collection = current_app.config["GENS_DB"]["annotations"]
-        sources = collection.distinct("source", {"hg_type": hg_type})
+        sources = collection.distinct("source", {"hg_type": str(hg_type)})
     return jsonify(status="ok", sources=sources)
 
 
@@ -140,22 +146,37 @@ def get_variant_data(region, hg_type, collapsed):
     )
 
 
-def get_multiple_coverages(case_id, hg_type, reduce_data, plot_height, top_bottom_padding,
-                           baf_y_start, baf_y_end, log2_y_start, log2_y_end, overview, **kwargs):
+def get_multiple_coverages(
+    case_id,
+    hg_type,
+    reduce_data,
+    plot_height,
+    top_bottom_padding,
+    baf_y_start,
+    baf_y_end,
+    log2_y_start,
+    log2_y_end,
+    overview,
+    **kwargs,
+):
     """Read default Log2 ratio and BAF values for overview graph."""
-    LOG.info(f'Got request for all chromosome coverages: {case_id}')
+    LOG.info(f"Got request for all chromosome coverages: {case_id}")
+    if connexion.request.is_json:
+        body = connexion.request.get_json()
+    return body, 201
+
     # open tabix filehandles
     cov_file, baf_file = get_tabix_files(
         case_id,
-        current_app.config[f"HG{hg_type}_PATH"]  # dir where cov files are stored
+        current_app.config[f"HG{hg_type}_PATH"],  # dir where cov files are stored
     )
-    data = kwargs.get('body', {})  # get request body
-    if 'chromosome_pos' not in data:
-        raise ValueError(f'Chromosome position not sent to API')
+    data = kwargs.get("body", {})  # get request body
+    if "chromosome_pos" not in data:
+        raise ValueError(f"Chromosome position not sent to API")
     results = {}
     for chrom_info in data["chromosome_pos"]:
         # Set some input values
-        chromosome = chrom_info['chromosome']
+        chromosome = chrom_info["chromosome"]
         req = REQUEST(
             f"{chromosome}:0-None",
             chrom_info["x_pos"],
@@ -204,23 +225,44 @@ def get_multiple_coverages(case_id, hg_type, reduce_data, plot_height, top_botto
     )
 
 
-def get_coverage(case_id, region, x_pos, y_pos, plot_height,
-                 top_bottom_padding, baf_y_start, baf_y_end,
-                 log2_y_start, log2_y_end, hg_type, reduce_data, x_ampl):
+def get_coverage(
+    case_id,
+    region,
+    x_pos,
+    y_pos,
+    plot_height,
+    top_bottom_padding,
+    baf_y_start,
+    baf_y_end,
+    log2_y_start,
+    log2_y_end,
+    hg_type,
+    reduce_data,
+    x_ampl,
+):
     """
     Reads and formats Log2 ratio and BAF values for overview graph
     Returns the coverage in screen coordinates for frontend rendering
     """
     # Validate input
-    if case_id == '':
+    if case_id == "":
         LOG.error(f"Invalid case_id: {case_id}")
         return abort(416)
 
     # Set some input values
     req = REQUEST(
-        region, x_pos, y_pos, plot_height, top_bottom_padding,
-        baf_y_start, baf_y_end, log2_y_start, log2_y_end,
-        hg_type, reduce_data)
+        region,
+        x_pos,
+        y_pos,
+        plot_height,
+        top_bottom_padding,
+        baf_y_start,
+        baf_y_end,
+        log2_y_start,
+        log2_y_end,
+        hg_type,
+        reduce_data,
+    )
 
     hg_filedir = current_app.config[f"HG{hg_type}_PATH"]
     cov_file, baf_file = get_tabix_files(case_id, hg_filedir)
