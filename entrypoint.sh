cd2f3d339872d89b9e77bdcc74c9c31a4d9506d5
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2020-12-18 08:56:42 +0100

    Updated default data mount

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,12 @@ set -e
 if [[ ! "${FLASK_ENV}" == development ]]; then
 				echo "Mount Lennart access drive"
 				mkdir -p "${HOME}/access"
-				sshfs                                                 \
-					-o reconnect,transform_symlinks,idmap=user          \
-					-o ro,ServerAliveInterval=30,ServerAliveCountMax=5  \
-					-o StrictHostKeyChecking=no                         \
-					-p 22                                               \
-					worker@lennart:/access "${HOME}/access"
+				sshfs																	\
+					-o reconnect,transform_symlinks,idmap=user							\
+					-o ro,ServerAliveInterval=30,ServerAliveCountMax=5					\
+					-o StrictHostKeyChecking=no											\
+					-p 22																\
+					worker@lennart:/media/isilon/backup_hopper/results "${HOME}/access"
 fi
 
 # run commands given to container
--- gens/config.py
+++ gens/config.py
@@ -1,8 +1,13 @@
+"""Gens default configuration."""
+# Database connection
 MONGODB_HOST = "mongodb"
 MONGODB_PORT = 27017
-HG19_PATH = "/access/wgs/plotdata"
-HG38_PATH = "/access/wgs/plot_data"
-# Colors for the UI
+GENS_DBNAME = 'gens'
+SCOUT_DBNAME = 'scout'
+# IO access for coverage files
+HG19_PATH = "/home/app/access/wgs/plot_data")
+HG38_PATH = "/home/app/access/wgs/plot_data")
+# UI colors
 UI_COLORS = {
     "variants": {"del": "#F94144", "dup": "#277DA1"},
     "transcripts": {"strand_pos": "#4D908E", "strand_neg": "#43AA8B"},
