57bfe16f364dc176abaf4010675477ef70d2d1b7
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2020-12-22 17:15:13 +0100

    Fixed error in default config

--- gens/config.py
+++ gens/config.py
@@ -5,8 +5,8 @@ MONGODB_PORT = 27017
 GENS_DBNAME = 'gens'
 SCOUT_DBNAME = 'scout'
 # IO access for coverage files
-HG19_PATH = "/home/app/access/wgs/plot_data")
-HG38_PATH = "/home/app/access/wgs/plot_data")
+HG19_PATH = "/home/app/access/wgs/plot_data"
+HG38_PATH = "/home/app/access/wgs/plot_data"
 # UI colors
 UI_COLORS = {
     "variants": {"del": "#F94144", "dup": "#277DA1"},
