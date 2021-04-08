3dd8a7baa772d7755cd26873a693e4d3f7c082bb
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2021-04-08 09:02:30 +0200

    Fixed bad js import

--- gulpfile.js
+++ gulpfile.js
@@ -1,21 +1,21 @@
 // requirements
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const rename = require('gulp-rename');
-const webpack = require('webpack');
-const sourcemaps = require('gulp-sourcemaps');
+const gulp = require('gulp')
+const sass = require('gulp-sass')
+const rename = require('gulp-rename')
+const webpack = require('webpack')
+const sourcemaps = require('gulp-sourcemaps')
 const resolve = require('path').resolve
-let webpackConfig = require('./webpack.config.js');
+const webpackConfig = require('./webpack.config.js')
 
 // define paths
 const dest = 'build'
 const assetPath = 'assets'
 const gensCss = [
-  `${assetPath}/css/gens.scss`,
+  `${assetPath}/css/gens.scss`
 ]
 
 // run webpack
-function runWebpack(config) {
+function runWebpack (config) {
   return new Promise((resolve, reject) => {
     webpack(config, (err, stats) => {
       if (err) {
@@ -30,73 +30,73 @@ function runWebpack(config) {
 }
 
 // PRODUCTION tasks
-gulp.task('build-js', function() {
+gulp.task('build-js', function () {
   return runWebpack(webpackConfig)
 })
 
-gulp.task('build-gens-css', function() {
+gulp.task('build-gens-css', function () {
   return gulp.src(gensCss)
     .pipe(rename('gens.min.css'))
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(`${dest}/css`))
-});
+})
 
-gulp.task('build-base-css', function() {
+gulp.task('build-base-css', function () {
   return gulp.src(`${assetPath}/css/base.scss`)
     .pipe(rename('base.min.css'))
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(`${dest}/css`))
-});
+})
 
-gulp.task('build-error-css', function() {
+gulp.task('build-error-css', function () {
   return gulp.src(`${assetPath}/css/error.scss`)
     .pipe(rename('error.min.css'))
     .pipe(sourcemaps.init())
-    .pipe(sass({outputStyle: 'compressed'}))
+    .pipe(sass({ outputStyle: 'compressed' }))
     .pipe(sourcemaps.write())
     .pipe(gulp.dest(`${dest}/css`))
-});
+})
 
 gulp.task('build', gulp.parallel('build-js', 'build-gens-css',
-                                 'build-base-css', 'build-error-css'));
+  'build-base-css', 'build-error-css'))
 
 // DEVELOPMENT tasks
 //
 const devGlobalAssets = 'gens/static'
 const devGensAssets = 'gens/blueprints/gens/static'
 gulp.task('build-js-dev', () => {
-  webpackConfig['mode'] = 'development'
-  webpackConfig['ouptut']['path'] = resolve('./gens/blueprints/gens/static/')
+  webpackConfig.mode = 'development'
+  webpackConfig.output.path = resolve('./gens/blueprints/gens/static/')
   return runWebpack(webpackConfig)
-});
+})
 
 gulp.task('build-gens-css-dev', () => {
   return gulp.src(gensCss)
     .pipe(rename('gens.min.css'))
     .pipe(sass())
     .pipe(gulp.dest(devGensAssets))
-});
+})
 
 gulp.task('build-base-css-dev', () => {
   return gulp.src(`${assetPath}/css/base.scss`)
     .pipe(rename('base.min.css'))
     .pipe(sass())
     .pipe(gulp.dest(`${devGlobalAssets}/css`))
-});
+})
 
 gulp.task('build-error-css-dev', () => {
   return gulp.src(`${assetPath}/css/error.scss`)
     .pipe(rename('error.min.css'))
     .pipe(sass())
     .pipe(gulp.dest(`${devGlobalAssets}/css`))
-});
+})
 
 gulp.task('watch', () => {
-  gulp.watch(`${assetPath}/css/*.scss`, gulp.parallel('build-gens-css-dev', 'build-base-css-dev', 'build-error-css-dev'));
+  gulp.watch(`${assetPath}/css/*.scss`, gulp.parallel('build-gens-css-dev', 'build-base-css-dev', 'build-error-css-dev'))
   gulp.watch(`${assetPath}/js/*.js`, gulp.parallel('build-js-dev'))
   gulp.watch(`${assetPath}/js/*/*.js`, gulp.parallel('build-js-dev'))
 })
