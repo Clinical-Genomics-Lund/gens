e05faa1765d0c438aa05f88c7c12e569d0aaad8a
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2021-06-02 16:29:36 +0200

    Refactored load annotation as cli command

--- gens/commands/load.py
+++ gens/commands/load.py
@@ -2,9 +2,10 @@ import click
 from flask import current_app as app
 from flask.cli import with_appcontext
 from gens.constants import HG_TYPE
-from gens.load import parse_chrom_sizes, build_transcripts
+from gens.load import parse_chrom_sizes, build_transcripts, parse_annotation_file, parse_annotation_entry, update_height_order, ParserError
 import logging
 from pymongo import ASCENDING
+from pathlib import Path
 
 LOG = logging.getLogger(__name__)
 
@@ -15,20 +16,61 @@ def load():
 
 @load.command()
 @click.option('-f', '--file', required=True,
-              type=click.File(),
-              help='Annotation file')
+              type=click.Path(exists=True),
+              help='File or directory of annotation files to load into the database')
 @click.option('-b', '--genome-build', type=click.Choice(HG_TYPE), required=True,
               help='Genome build')
 @click.option('-u', '--update', is_flag=True,
               help="Update existing database with new information")
+@with_appcontext
 def annotations(file, genome_build, update):
     """Load annotations from file into the database."""
-    pass
+    COLLECTION = 'annotations'
+    # check if path is a directoy of a file
+    path = Path(file)
+    files = path.glob('*') if path.is_dir() else [path]
+    db = app.config['GENS_DB'][COLLECTION]
+    LOG.info('Create indexes')
+    for param in ['start', 'end', 'chrom', 'source', 'height_order']:
+        db.create_index([(param, ASCENDING)], unique=False)
+    db.create_index("hg_type", unique=False)
+    LOG.info('Processing files')
+    for annot_file in files:
+        # verify file format
+        if annot_file.suffix in ['.bed', '.aed']:
+            click.UsageError('Invalid file format, expects either a bed or aed file')
+        # base the annotation name on the filename
+        annotation_name = annot_file.name[:-len(annot_file.suffix)]
+        try:
+            parser = parse_annotation_file(annot_file, genome_build, format=annot_file.suffix[1:])
+            annotation_obj = []
+            for entry in parser:
+                try:
+                    entry_obj = parse_annotation_entry(entry, genome_build, annotation_name)
+                    annotation_obj.append(entry_obj)
+                except ParserError as err:
+                    LOG.warning(str(err))
+                    continue
+
+        except Exception as err:
+            LOG.error(f'{str(err)}')
+            raise click.UsageError(str(err))
+
+        # Remove existing annotations in database
+        LOG.info(f'Remove old entry in the database')
+        db.remove({"source": annotation_name})
+        # add the annotations
+        LOG.info(f'Load annoatations in the database')
+        db.insert_many(annotation_obj)
+        LOG.info('Update height order')
+        # update the height order of annotations in the database
+        update_height_order(db, annotation_name)
+
 
 
 @load.command()
-@click.option('-f', '--file', required=True,
-              type=click.File(),
+@click.option('-f', '--file',
+              type=click.Path(exists=True),
               help='Transcript file')
 @click.option('-m', '--mane', type=click.File(), required=True,
               help='Mane file')
@@ -49,10 +91,8 @@ def transcripts(file, mane, genome_build, update):
     db = app.config['GENS_DB'][COLLECTION]
     db.insert_many(transcripts)
     LOG.info('Create indexes')
-    db.create_index([("start", ASCENDING)], unique=False)
-    db.create_index([("end", ASCENDING)], unique=False)
-    db.create_index([("chrom", ASCENDING)], unique=False)
-    db.create_index([("height_order", ASCENDING)], unique=False)
+    for param in ['start', 'end', 'chrom', 'hg_type']:
+        db.create_index([(param, ASCENDING)], unique=False)
     db.create_index("hg_type", unique=False)
     click.secho("Finished loading transcripts ✔", fg="green")
 
--- gens/load/__init__.py
+++ gens/load/__init__.py
@@ -1,2 +1,3 @@
 from .chrom_sizes import parse_chrom_sizes
+from .annotations import parse_annotation_file, parse_annotation_entry, update_height_order, ParserError
 from .transcripts import build_transcripts
