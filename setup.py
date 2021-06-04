ea00377cce8cd2f83fd89754b0737133ad1794ac
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2021-06-04 09:11:03 +0200

    Specify python 3.9 support in setup.py

--- setup.py
+++ setup.py
@@ -19,6 +19,7 @@ setup(
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
     ],
     keywords=["Flask", "Genomics", "WGS"],
     packages=find_packages(),
