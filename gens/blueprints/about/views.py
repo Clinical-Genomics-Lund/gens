73abfc6910d419cec53bddf987fc59b7b403dde4
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2021-06-07 17:32:08 +0200

    Display timestamps of data in /about

--- gens/blueprints/about/views.py
+++ gens/blueprints/about/views.py
@@ -2,16 +2,26 @@
 
 import logging
 
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, current_app
 
-import gens
+from gens import version
+from gens.db import get_timestamps
+from itertools import groupby
 
 LOG = logging.getLogger(__name__)
 
-about_bp = Blueprint("about", __name__, template_folder="templates")
+about_bp = Blueprint(
+        "about",
+        __name__, 
+        template_folder="templates", 
+        static_folder="static",
+        static_url_path="/about/static",
+)
 
 # define views
 @about_bp.route("/")
 @about_bp.route("/about")
 def home():
-    return render_template("about.html", version=gens.version)
+    with current_app.app_context():
+        timestamps = get_timestamps()
+    return render_template("about.html", timestamps=timestamps, version=version)
