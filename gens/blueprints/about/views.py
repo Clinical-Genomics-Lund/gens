59ceea250645d60938863a165aa557a90e18a790
Markus Johansson <markjo@MTLUCMDS2.lund.skane.se>
2021-06-08 11:06:07 +0200

    About displays current config and whats in db

--- gens/blueprints/about/views.py
+++ gens/blueprints/about/views.py
@@ -10,6 +10,17 @@ from itertools import groupby
 
 LOG = logging.getLogger(__name__)
 
+IN_CONFIG = (
+        'ENV', 
+        'HG19_PATH',
+        'HG38_PATH',
+        'DEFAULT_ANNOTATION_TRACK',
+        'GENS_DBNAME',
+        'SCOUT_DBNAME',
+        'MONGODB_HOST',
+        'MONGODB_PORT',
+)
+
 about_bp = Blueprint(
         "about",
         __name__, 
@@ -18,10 +29,14 @@ about_bp = Blueprint(
         static_url_path="/about/static",
 )
 
+
 # define views
 @about_bp.route("/")
 @about_bp.route("/about")
 def home():
     with current_app.app_context():
         timestamps = get_timestamps()
-    return render_template("about.html", timestamps=timestamps, version=version)
+        config = {cnf: current_app.config.get(cnf) for cnf in IN_CONFIG}
+        ui_colors = current_app.config.get("UI_COLORS")
+    return render_template("about.html", timestamps=timestamps,
+            version=version, config=config, ui_colors=ui_colors)
