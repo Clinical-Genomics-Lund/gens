fd7a6c0b3bbd7c369255a6431ebf2365b8a2cc3d
mhkc <markus.johansson@me.com>
2023-11-16 14:36:01 +0100

    added api mongodb interface module

---
+++ api/app/db/__init__.py
@@ -0,0 +1,3 @@
+"""Mongo database interface."""
+
+from .utils import connect_to_mongo, close_mongo_connection
---
+++ api/app/main.py
@@ -0,0 +1,33 @@
+"""Gens API entrypoint."""
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from .config import allowed_origins
+from .db import close_mongo_connection, connect_to_mongo
+
+
+def configure_cors(app):
+    """Configure cros site resource sharing for API.
+
+    configuration is only applied if there are allowed origins specified in config
+    """
+    if len(allowed_origins) > 0:
+        app.add_middleware(
+            CORSMiddleware,
+            allow_origins=allowed_origins,
+            allow_credentials=True,
+            allow_methods=["*"],
+            allow_headers=["*"],
+        )
+
+
+# Setup API
+app = FastAPI(title="Gens")
+
+# configure events
+app.add_event_handler("startup", connect_to_mongo)
+app.add_event_handler("shutdown", close_mongo_connection)
+
+# add api routes
+# app.include_router(root.router)
