22bd85b2b137866ffe79674ba8bf1bb44a64577e
Daniel Nilsson <daniel.k.nilsson@gmail.com>
2024-02-22 12:05:40 +0100

    addition is hard :)

--- assets/js/navigation.js
+++ assets/js/navigation.js
@@ -158,8 +158,8 @@ export function panTracks(direction = 'left', speed = 0.1) {
     pos.end += distance
   }
   // drawTrack will correct the window eventually, but let us not go negative at least
-  if (pos.start < 1) {
-    pos.end = distance - pos.start
+  if (pos.start < 0) {
+    pos.end = pos.end + distance - pos.start
     pos.start = 1
   }
   drawTrack({ chrom: pos.chrom, start: pos.start, end: pos.end, drawTitle: false, exclude: ['cytogenetic-ideogram'] })
