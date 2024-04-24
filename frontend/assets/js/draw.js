9af7e056ff8b6791d8c654edee6be117a391d97a
mhkc <markus.johansson@me.com>
2024-04-24 16:34:28 +0200

    Cleanup unnessecary files

--- frontend/assets/js/draw.js
+++
@@ -1,4 +0,0 @@
-// entrypoint for drawing functions
-
-export { drawVerticalTicks, createGraph, drawGraphLines } from './draw/graph.js'
-export { drawRotatedText, drawPoints, drawText, drawLine, drawRect, drawArrow, drawWaveLine } from './draw/shapes.js'
